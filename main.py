17b58cddfeb467df2c697a1fcaac13df60417973
Dlotsss <140627941+dlotsss@users.noreply.github.com>
2025-02-10 20:54:49 +0500

    uploaded characteristics

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ from random import *
 import time
 import pygame
 pygame.init()
+
+# кадры в секунду
+fps = 40
+
+#текст для отображения 
+TEXTCARD = 'CLICK'
+TIME = 'Время:'
+COUNTER = 'Счет'
+
+#размер текста
+size = 50
+
